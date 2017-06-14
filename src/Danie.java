6247a02338f9f3cfb415718b44ebca01210d9d29
Greg <krainamodu@gmail.com>
2017-06-15 01:00:13 +0200

    commit1

---
+++ src/Danie.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Grzeg on 12.06.2017.
+ */
+public class Danie {
+    private String nazwa;
+    private double porcje;
+    private double cena;
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public double getPorcje() {
+        return porcje;
+    }
+
+    public double getCena() {
+        return cena;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public void setPorcje(double porcje) {
+        this.porcje = porcje;
+    }
+
+    public void setCena(double cena) {
+        this.cena = cena;
+    }
+}
